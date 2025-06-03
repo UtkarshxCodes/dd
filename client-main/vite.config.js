bf051fc4ce5e6f8efd01b8bf6967ce1decf9121d
Utkarsh Chaudhary <utkarshjaat01@gmail.com>
2025-06-03 06:08:43 +0530

    hh

--- client-main/vite.config.js
+++ client-main/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import glslify from 'vite-plugin-glslify';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), glslify()],
+});
